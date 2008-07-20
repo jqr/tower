543b58053bf5b88587adaa421214679eb887bffd
Elijah Miller <elijah.miller@gmail.com>
2008-07-20 03:00:57 -0400

    Using super in ExplosiveProjectile initialize.

--- lib/explosive_projectile.rb
+++ lib/explosive_projectile.rb
@@ -2,14 +2,9 @@ class ExplosiveProjectile < Projectile
   attr_accessor :x, :y, :damage
 
   def initialize(window, x, y, speed, enemy)
-    @image = Gosu::Image::load_tiles(window, "images/projectile.png", 25, 25, false).first
-    @x = x
-    @y = y
-    @speed = speed
-    @window = window
+    super
     @damage = 10 + rand(10)
     @size = 1
-    attacking(enemy)
   end
   
   def draw
