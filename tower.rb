474b04306970c81ca29d4b390aa8f51530b6fa67
Elijah Miller <elijah.miller@gmail.com>
2008-07-19 20:06:07 -0400

    Stubbed out draw methods.

--- tower.rb
+++ tower.rb
@@ -3,26 +3,41 @@ require 'gosu'
 
 class Tower
   attr_accessor :x, :y
+  
+  def draw
+  end
 end
 
 class Projectile
   attr_accessor :x, :y
+
+  def draw
+  end
 end
 
 class Enemy
   attr_accessor :x, :y, :distance
+
+  def draw
+  end
 end
 
 class GameWindow < Gosu::Window
   def initialize
     super(640, 480, false)
     self.caption = "Tower"
+    @towers = []
+    @projectiles = []
+    @enemies = []
   end
 
   def update
   end
 
   def draw
+    (@towers + @projectiles + @enemies).each do |object|
+      object.draw
+    end
   end
 end
 
