eebd903641dbc42bdfa778c1c9eeaeac54e75b46
Elijah Miller <elijah.miller@gmail.com>
2013-03-20 00:47:16 -0400

    Whitespace.

--- lib/enemy.rb
+++ lib/enemy.rb
@@ -1,6 +1,6 @@
 class Enemy
   include Drawable
-  
+
   attr_accessor :x, :y, :health, :image, :heading
 
   def initialize(window, x, y)
@@ -24,7 +24,7 @@ class Enemy
       @new_tile = true
     end
   end
-    
+
   def draw
     @frame += 1
     @image.draw_rot(x + @image.width / 2, y + @image.height / 2, 0, (@frame / 10) % 2 * - 30 + 35, 0.5, 0.5)
@@ -34,29 +34,29 @@ class Enemy
       highlight_tile
     end
   end
-  
+
   def draw_health
     @font.draw(@health.to_s, x - 8, y - 23, 0, 1, 1, 0xffffff00)
   end
-  
+
   def move!
     calculate_heading if recalculate_heading?
 
     self.x, self.y = next_move(heading)
   end
-  
+
   def recalculate_heading?
     @new_tile == true && @window.board.inside_tile?(x, y, image.width / 2)
   end
-  
+
   def calculate_heading
     @new_tile = false
-    
+
     cells = @window.board.surrounding_cells(board_x, board_y)
 
     min_distance = cells.select { |c| c.is_a?(Integer) }.min
-    
-    self.heading = 
+
+    self.heading =
       case cells.index(min_distance)
         when 0:
           :down
@@ -68,7 +68,7 @@ class Enemy
           :up
       end
   end
-  
+
   def next_move(direction)
     case direction
       when :down
@@ -81,15 +81,15 @@ class Enemy
         [x + speed, y]
     end
   end
-  
+
   def speed
     0.3
   end
-  
+
   def position
     [x, y]
   end
-  
+
   def hit(projectile)
     @health -= projectile.damage
     destroy if @health <= 0
@@ -98,14 +98,14 @@ class Enemy
   def destroy
     @window.remove_enemy(self)
   end
-  
+
   def exit_event
     if y > @window.height
       @window.remove_enemy(self)
       @window.increment_enemies_exited
     end
   end
-  
+
   def credit_value
     15
   end
